ac33eec04584dd382e1016559bd9573b49dff4fa
knownout <a.slavinskii@icloud.com>
2025-02-12 12:36:52 +0300

    Move options to separate object & update createFallbackProvider function

--- src/ethers-provider/create-fallback-provider.ts
+++ src/ethers-provider/create-fallback-provider.ts
@@ -1,34 +1,53 @@
 import { FallbackProvider, JsonRpcProvider, Network } from "ethers"
 import publicNodesList from "./public-nodes-list"
 
+interface Options {
+  /** List of custom nodes to use */
+  attachNodes?: string[]
+
+  /** Prioritize custom nodes over default public nodes */
+  prioritizeAttached?: boolean
+
+  /** FallbackProvider stallTimeout option */
+  stallTimeout?: number
+
+  /** FallbackProvider stallTimeout option */
+  quorum?: number
+
+  /** Set node weight based on priority (1 for default nodes and 2 for prioritized) */
+  priorityBasedWeight?: boolean
+}
+
 /**
  * Create ethers FallbackProvider from public provider URLs
  *
  * Injects a private node at the top (priority = 1) if configured in the environment
  *
- * @param network network
- * @param attachNodes optional list of additional nodes
- * @param prioritizeAttached prioritize attached nodes over default public nodes, false by default
- * @param stallTimeout
+ * @param network provider network
+ * @param options provider options
  */
-export default function createFallbackProvider(network: Network, attachNodes?: string[], prioritizeAttached = false, stallTimeout = 10_000) {
+export default function createFallbackProvider(network: Network, options: Options) {
   // Get a list of public provider URLs
   const publicNetworkProviders = publicNodesList.get(network.name)
 
   if (!publicNetworkProviders) throw new Error("Unsupported network: " + network.name)
 
-  const networkProviders = [ ...publicNetworkProviders, ...(attachNodes ?? []) ]
+  const networkProviders = [...publicNetworkProviders, ...(options.attachNodes ?? [])]
     .filter(providerUrl => providerUrl.length > 0)
 
   // Create the fallback provider
   return new FallbackProvider(networkProviders.map((providerUrl, index) => {
-    const isPrioritized = attachNodes !== undefined && attachNodes.includes(providerUrl) && prioritizeAttached
+    const isPrioritized = options.attachNodes !== undefined
+      && options.attachNodes.includes(providerUrl)
+      && options.prioritizeAttached === true
+
+    const prioritizedWeight = options.attachNodes ? (options.attachNodes.includes(providerUrl) && options.prioritizeAttached ? 1 : 1) : 1
 
     return {
       provider: new JsonRpcProvider(providerUrl, network.chainId, { staticNetwork: network }),
       priority: (index + 11) - (isPrioritized ? 10 : 0),
-      stallTimeout
-      //weight: attachNodes ? (attachNodes.includes(providerUrl) && prioritizeAttached ? 1 : 1) : 1
+      stallTimeout: options.stallTimeout,
+      weight: options.priorityBasedWeight ? prioritizedWeight : undefined
     }
-  }))
+  }), network, { quorum: options.quorum ?? 1 })
 }
