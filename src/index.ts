4e79c9a4a11765d2fc35e51c3edc45e58d600c57
knownout <a.slavinskii@icloud.com>
2025-07-22 16:49:57 +0300

    Add units into export

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import ethersProvider, { reconfigureProvidersList } from "./ethers-provider"
 import { MultiCallRequest, MultiCallResponse, Call3_MultiCallStruct } from "./types"
 import arrayUtils from "./utils/array-utils"
 import cast from "./utils/cast"
-import { arbitrum, avalanche, bnb, mainnet, matic, tron, evmNetworksList, optimism, gnosis, scroll, networksList, base } from "./networks"
+import { arbitrum, avalanche, bnb, mainnet, matic, tron, evmNetworksList, optimism, gnosis, scroll, networksList, base, units } from "./networks"
 import multicall from "./utils/multicall"
 import selectAddress from "./utils/select-address"
 
@@ -21,6 +21,7 @@ export {
   tron,
   evmNetworksList,
   networksList,
+  units,
 
   arrayUtils,
   multicall,
